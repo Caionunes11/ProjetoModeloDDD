63550eb6224b73cf664c3421996a849ec6b22540
Caio Nunes <caio.nunes@minutoseguros.com.br>
2022-03-24 21:40:08 -0300

    Add project files.

---
+++ src/ProjetoModelo.Infra.Data/ProjetoModelo.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModelo.Infra.Data.Repositories
+{
+    public class ProdutoRepository : RepositoryBase<Produto>
+    {
+
+    }
+}
---
+++ src/ProjetoModeloDDD.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProjetoModelo.Domain.Entities;
+
+namespace ProjetoModeloDDD.Domain.Interfaces
+{
+    public interface IClienteRepository : IRepositoryBase<Cliente>
+    {
+
+    }
+}
